9c2e24cc3e1d7595723ba59dd43b24ca042daadb
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-05-23 15:25:35 +0000

    Configure axios interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios"
 axios.defaults.baseURL = 'https://8000-dym077-drfapi-5irdrexgzy1.ws-eu114.gitpod.io'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
